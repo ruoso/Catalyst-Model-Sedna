10882c13e04812d98f9a8470637e0ac003c6b8b1
Daniel Ruoso <daniel@ruoso.com>
2010-04-06 14:31:39 -0300

    Preparing for a release

--- lib/Catalyst/Model/Sedna.pm
+++ lib/Catalyst/Model/Sedna.pm
@@ -2,49 +2,60 @@ package Catalyst::Model::Sedna;
 use Sedna;
 use Moose;
 
+our $VERSION = 0.001;
+
 extends 'Catalyst::Model';
-
-has 'conn' => (is => 'rw',
+has 'conn' => (is => 'ro',
                isa => 'Sedna',
-               handles => [ 'execute',
-                            'begin',
-                            'rollback',
-                            'getData',
-                            'next',
-                            'commit']);
+               required => 1,
+               handles => [qw(execute begin
+                              rollback getData
+                              next commit )]);
 
-sub new {
-  my $self = shift;
-  $self = $self->SUPER::new(@_);
-  my ($c, $config) = @_;
+__PACKAGE__->meta->make_immutable;
 
-  my $conn =
-    Sedna->connect($config->{url},
-               $config->{db_name},
-               $config->{login},
-               $config->{password});
+sub BUILDARGS {
+    my ($self, $c, $config) = @_;
 
-  $conn->setConnectionAttr(%{$config->{attr}})
-    if $config->{attr};
+    my $conn =
+      Sedna->connect($config->{url},
+                     $config->{db_name},
+                     $config->{login},
+                     $config->{password});
 
-  $self->conn($conn);
-  return $self;
+    $conn->setConnectionAttr(%{$config->{attr}})
+      if exists $config->{attr} &&
+        ref $config->{attr} eq 'HASH';
+
+    return { conn => $conn };
 }
 
 sub get_item {
-  my $self = shift;
-  if ($self->conn->next) {
-    my $buf = ' 'x1024; # create the scalar in the size we want.
-    my $ret = '';
-    while (my $read = $self->conn->getData($buf, 1024)) {
-      $ret .= substr($buf,0,$read);
+    my $self = shift;
+    if ($self->conn->next) {
+        my ($buf,$ret);
+        while (my $read = $self->conn->getData($buf, 1024)) {
+            $ret .= substr($buf,0,$read);
+        }
+        return $ret;
+    } else {
+        return;
     }
-    return $ret;
-  } else {
-    return;
-  }
 }
 
+
+sub get_document {
+    my ( $self, $doc ) = @_;
+    $self->execute( 'for $x in doc("' . $doc . '") return $x' );
+    my $data = $self->get_item;
+    return $data;
+}
+
+sub store_document {
+    my ( $self, $xml, $doc_id, $collection ) = @_;
+    $self->conn->loadData( $xml, $doc_id, $collection );
+    $self->conn->endLoadData();
+}
 
 42;
 
@@ -56,12 +67,56 @@ Catalyst::Model::Sedna - Access the Sedna XML Database
 
 =head1 SYNOPSIS
 
-  TODO
+  package MyApp::Model::Sedna;
+  use base 'Catalyst::Model::Sedna;
+  __PACKAGE__->config({ url => 'localhost',
+                        db_name => 'mydb',
+                        user => 'myuser',
+                        password => 'password' });
+
+  # later in your application
+  my $s = $c->model('Sedna');
+  $s->execute('for $x in document("doc123") return $x');
+  if (my $item = $s->get_item()) {
+    $c->res->content_type('application/xml');
+    $c->res->body($item);
+  }
 
 =head1 DESCRIPTION
 
 This module will manage a connection to the sedna database and perform
-queries. The connection attributes are set in the config file.
+queries. The connection attributes are set in the config file. Note
+that the Sedna connection does not support cursors, so if you need to
+insert data while traversing another query, you need a second
+connection.
+
+=head1 METHODS
+
+=over
+
+=item conn
+
+Returns the Sedna connection object.
+
+=item execute/begin/rollback/getData/next/commit
+
+Methods handled directly by the connection.
+
+=item get_item
+
+This is a convenience method that will see if there is a next item
+available in the connection and already fetches the entire result to a
+scalar value.
+
+=item get_document($id)
+
+This method will do a simple query to fetch a document by its id and
+return its content.
+
+=item store_document($xml, $id, $collection)
+
+This method will load the xml data sent into a document of the given
+id. Optionally a collection can be sent.
 
 =head1 SEE ALSO
 
